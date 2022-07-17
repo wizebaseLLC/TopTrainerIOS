4eec06c9ae4ab83e9dd9b62e24c94010ecd19cb2
servon <servonlewis@gmail.com>
2022-07-17 19:21:07 -0400

    Initial Commit

---
+++ Top Trainer/Top_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Top_TrainerApp.swift
+//  Top Trainer
+//
+//  Created by Servon Lewis on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Top_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
